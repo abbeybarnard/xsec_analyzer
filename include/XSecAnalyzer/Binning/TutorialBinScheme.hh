5d63f8c1546274e6fda91edff78f12d6967eebca
Abbey Barnard <abbey.barnard@physics.ox.ac.uk>
2026-05-11 20:14:09 -0500

    Help

--- include/XSecAnalyzer/Binning/TutorialBinScheme.hh
+++ include/XSecAnalyzer/Binning/TutorialBinScheme.hh
@@ -3,6 +3,10 @@
 // XSecAnalyzer includes
 #include "XSecAnalyzer/Binning/BinSchemeBase.hh"
 
+// Constants removed from framework — define locally
+constexpr int kSignalTrueBin = 0;
+constexpr int kOrdinaryRecoBin = 0;
+
 class TutorialBinScheme : public BinSchemeBase {
 
   public:
